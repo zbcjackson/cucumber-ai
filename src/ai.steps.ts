d7e12ecd100d4436fa8bce411680a4e4403925aa
Jackson Zhang <zbcjackson@gmail.com>
2025-07-20 13:06:01 +0800

    :recycle: separate setup and steps

--- src/ai.steps.ts
+++ src/ai.steps.ts
@@ -1,13 +1,6 @@
-import {Before, defineStep, setDefaultTimeout, setWorldConstructor} from "@cucumber/cucumber";
+import {defineStep} from "@cucumber/cucumber";
 import { AgentWorld } from './agent.world';
-
-setWorldConstructor(AgentWorld);
-setDefaultTimeout(600 * 1000);
 
 defineStep(/^(.*)$/, async function (this: AgentWorld, stepText: string) {
   await this.agent.executeStep(stepText);
 });
-
-Before(async function (this: AgentWorld) {
-    await this.agent.start();
-})
---
+++ src/setup.ts
@@ -0,0 +1,9 @@
+import {Before, setDefaultTimeout, setWorldConstructor} from "@cucumber/cucumber";
+import {AgentWorld} from "./agent.world";
+
+setWorldConstructor(AgentWorld);
+setDefaultTimeout(600 * 1000);
+
+Before(async function (this: AgentWorld) {
+    await this.agent.start();
+})
