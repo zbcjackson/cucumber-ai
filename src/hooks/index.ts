0cb3e41c970db7c92c860faf3cc25f922bf8a846
Jackson Zhang <zbcjackson@gmail.com>
2025-07-26 23:02:46 +0800

    :sparkles: support after hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,21 @@
-import { Before } from "@cucumber/cucumber";
+import { After, Before } from "@cucumber/cucumber";
 import { AgentWorld } from "../agent.world";
 import { loadHooks } from "../loaders/hook-loader";
 
 export function setupHooks() {
   const hooks = loadHooks();
   for (const hook of hooks) {
-    Before(hook.tags, async function (this: AgentWorld) {
-      await this.executeActions(hook.actions);
-    });
+    switch (hook.hook) {
+      case "before":
+        Before(hook.tags, async function (this: AgentWorld) {
+          await this.executeActions(hook.actions);
+        });
+        break;
+      case "after":
+        After(hook.tags, async function (this: AgentWorld) {
+          await this.executeActions(hook.actions);
+        });
+        break;
+    }
   }
 }
