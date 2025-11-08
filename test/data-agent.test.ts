c9bfebf23e135af61d6732964517afebff742682
Jackson Zhang <zbcjackson@gmail.com>
2025-11-08 11:19:26 +0800

    :arrow_up: upgrade dependencies and fix tests definition

--- test/data-agent.test.ts
+++ test/data-agent.test.ts
@@ -15,29 +15,21 @@ describe.skip("data agent", () => {
     const result = await dataAgent.ask("SELECT 1 + 1 AS result");
     expect(result).toEqual({ success: true, result: { result: 2 } });
   });
-  it(
-    "api invocation",
-    async () => {
-      const result = await dataAgent.ask("get verification code from the last mail to zbcjackson@proton.me");
-      expect(result).toEqual({ success: true, result: { verificationCode: "USEXRQ" } });
-    },
-    { timeout: 10000 },
-  );
-  it(
-    "cookies should be set",
-    async () => {
-      await dataAgent.ask("send verification code mail to zbcjackson@proton.me");
-      const {
-        result: { verificationCode: code },
-      } = await dataAgent.ask("get verification code from the last mail to zbcjackson@proton.me");
-      let result = await dataAgent.ask(`login with email zbcjackson@proton.me and verification code ${code}`);
-      expect(result.success).toBeTruthy();
+  it("api invocation", { timeout: 10000 }, async () => {
+    const result = await dataAgent.ask("get verification code from the last mail to zbcjackson@proton.me");
+    expect(result).toEqual({ success: true, result: { verificationCode: "USEXRQ" } });
+  });
+  it("cookies should be set", { timeout: 100000 }, async () => {
+    await dataAgent.ask("send verification code mail to zbcjackson@proton.me");
+    const {
+      result: { verificationCode: code },
+    } = await dataAgent.ask("get verification code from the last mail to zbcjackson@proton.me");
+    let result = await dataAgent.ask(`login with email zbcjackson@proton.me and verification code ${code}`);
+    expect(result.success).toBeTruthy();
 
-      result = await dataAgent.ask("complete user info with nick name Jackson");
-      expect(result.success).toBeTruthy();
-      result = await dataAgent.ask(`create team with name "My Team", indicator name "Revenue", indicator value 1000`);
-      expect(result.success).toBeTruthy();
-    },
-    { timeout: 100000 },
-  );
+    result = await dataAgent.ask("complete user info with nick name Jackson");
+    expect(result.success).toBeTruthy();
+    result = await dataAgent.ask(`create team with name "My Team", indicator name "Revenue", indicator value 1000`);
+    expect(result.success).toBeTruthy();
+  });
 });
